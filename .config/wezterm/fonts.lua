18703ade8fbdacf1dd135263f1d1ecc9a192c2ac
Yuri Barsotti <yuribarsotti@gmail.com>
2024-12-04 16:48:39 -0300

    Update fonts.lua

--- .config/wezterm/fonts.lua
+++ .config/wezterm/fonts.lua
@@ -4,14 +4,16 @@ local M = {}
 
 function M.setup(config)
 	config.font = wezterm.font_with_fallback({
-		"MonoLisa",
+		"Dank Mono",
 		"Fira Code",
 		"Jetbrains Mono",
 	})
+	config.font_size = 14.0
+	config.line_height = 1.1
 	config.adjust_window_size_when_changing_font_size = false
 	config.window_frame = {
-		font = wezterm.font({ family = "Noto Sans", weight = "Regular" }),
-		font_size = 12.0,
+		font = wezterm.font({ family = "Dank Mono", weight = "Regular" }),
+		font_size = 14.0,
 	}
 end
 
